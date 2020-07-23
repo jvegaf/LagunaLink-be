b9a4431a3bec41d73ef4aec7724125eeeaa24053
Jose Vega <josevega234@gmail.com>
2020-07-23 12:24:12 +0200

    bugfix

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/api/language", languageCtrl.getLanguages());
-app.get("/api/language/:laguageId", languageCtrl.getLanguage());
-app.post("/api/language", languageCtrl.saveLanguage());
-app.delete("/api/language/:id", languageCtrl.deleteLanguage());
+app.get("/api/language", languageCtrl.getLanguages);
+app.get("/api/language/:laguageId", languageCtrl.getLanguage);
+app.post("/api/language", languageCtrl.saveLanguage);
+app.delete("/api/language/:id", languageCtrl.deleteLanguage);
 
 
 module.exports = app;
