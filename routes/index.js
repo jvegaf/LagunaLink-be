a7276c44b3876232d26d1fc935f67ffc955f1688
Jose Vega <josevega234@gmail.com>
2020-07-24 14:53:10 +0200

    added my first auth middleware

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const api = express.Router();
+const auth = require('../middlewares/auth');
 const languageCtrl = require('../controllers/language');
 
-api.get("/language", languageCtrl.getLanguages);
-api.get("/language/:laguageId", languageCtrl.getLanguage);
-api.post("/language", languageCtrl.saveLanguage);
-api.delete("/language/:id", languageCtrl.deleteLanguage);
 
+/** Language */
+api.get('/language', languageCtrl.getLanguages);
+api.get('/language/:laguageId', languageCtrl.getLanguage);
+api.post('/language', languageCtrl.saveLanguage);
+api.delete('/language/:id', languageCtrl.deleteLanguage);
+/** User */
+// api.post('/user/sign_in')
+/** Student */
+api.post('/register/student', auth.isAuth, (req, res) => {
+   res.status(200).send({ message: 'user loged' }); 
+});
 module.exports = api;
