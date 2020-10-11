cfcc6808a4aa2fdd6b7b1afa67dd5b5c6f049815
Jose Vega <josevega234@gmail.com>
2020-10-12 01:25:44 +0200

    agregado modulo cucumber-pretty

--- cucumber.js
+++ cucumber.js
@@ -1,7 +1,8 @@
 let common = [
   'tests/app/features/**/*.feature', // Specify our feature files
   '--require-module ts-node/register', // Load TypeScript module
-  '--require tests/app/features/step_definitions/*.steps.ts' // Load step definitions
+  '--require tests/app/features/step_definitions/*.steps.ts', // Load step definitions
+  '--format node_modules/cucumber-pretty' // Load custom formatter
 ].join(' ');
 
 module.exports = {
