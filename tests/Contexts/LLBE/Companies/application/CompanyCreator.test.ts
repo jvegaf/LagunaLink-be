b316966cec999f0c05995f180767b172b1a747f2
Jose Vega <josevega234@gmail.com>
2020-10-17 23:55:58 +0200

    bugfix mockrepository

--- tests/Contexts/LLBE/Companies/application/CompanyCreator.test.ts
+++ tests/Contexts/LLBE/Companies/application/CompanyCreator.test.ts
@@ -1,12 +1,10 @@
-import {CompanyRepositoryMock} from "../__mocks__/CompanyRepositoryMock";
-import {CompanyCreator} from "../../../../../src/Contexts/LLBE/Companies/application/CompanyCreator";
-import {CreateCompanyRequestMother} from "./CreateCompanyRequestMother";
-import {CompanyMother} from "../domain/CompanyMother";
-
+import { CompanyRepositoryMock } from '../__mocks__/CompanyRepositoryMock';
+import { CompanyCreator } from '../../../../../src/Contexts/LLBE/Companies/application/CompanyCreator';
+import { CreateCompanyRequestMother } from './CreateCompanyRequestMother';
+import { CompanyMother } from '../domain/CompanyMother';
 
 let repository: CompanyRepositoryMock;
 let creator: CompanyCreator;
-
 
 beforeEach(() => {
   repository = new CompanyRepositoryMock();
@@ -17,7 +15,7 @@ it('should create a valid company', async () => {
   const request = CreateCompanyRequestMother.random();
 
   const company = CompanyMother.fromRequest(request);
-
+  repository.whenSearchThenReturn(null);
   await creator.run(request);
 
   repository.assertLastSavedCompanyIs(company);
