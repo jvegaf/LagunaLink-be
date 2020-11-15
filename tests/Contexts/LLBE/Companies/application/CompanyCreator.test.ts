6893aaa1d52615aa316beb018ba4c21f18ba14d2
Jose Vega <josevega234@gmail.com>
2020-11-15 21:18:46 +0100

    Test Refactored

--- tests/Contexts/LLBE/Companies/application/CompanyCreator.test.ts
+++ tests/Contexts/LLBE/Companies/application/CompanyCreator.test.ts
@@ -2,13 +2,18 @@ import { CompanyRepositoryMock } from '../__mocks__/CompanyRepositoryMock';
 import { CompanyCreator } from '../../../../../src/Contexts/LLBE/Companies/application/CompanyCreator';
 import { CreateCompanyRequestMother } from './CreateCompanyRequestMother';
 import { CompanyMother } from '../domain/CompanyMother';
+import { UserUpdateRegistered } from '../../../../../src/Contexts/LLBE/Users/application/UserUpdateRegistered';
+import { UserUpdateRegisteredMock } from '../../Shared/__mocks__/UserUpdateRegisteredMock';
+import { UserRepositoryMock } from '../../Users/__mocks__/UserRepositoryMock';
 
 let repository: CompanyRepositoryMock;
+let userUpdRegMock: UserUpdateRegistered;
 let creator: CompanyCreator;
 
 beforeEach(() => {
+  userUpdRegMock = new UserUpdateRegisteredMock(new UserRepositoryMock());
   repository = new CompanyRepositoryMock();
-  creator = new CompanyCreator(repository);
+  creator = new CompanyCreator(repository, userUpdRegMock);
 });
 
 it('should create a valid company', async () => {
