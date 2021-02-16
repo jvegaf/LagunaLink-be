1b8075c6a2e9ef5dbbeffc138b89d4f2f4b15f5e
Jose Vega <josevega234@gmail.com>
2021-02-16 16:43:03 +0100

    HotFix validations

--- src/Contexts/LLBE/Companies/domain/CompanyName.ts
+++ src/Contexts/LLBE/Companies/domain/CompanyName.ts
@@ -4,12 +4,5 @@ import { InvalidArgumentError } from '../../../Shared/domain/value-object/Invali
 export class CompanyName extends StringValueObject {
   constructor(value: string) {
     super(value);
-    this.ensureLengthIsLessThan30Characters(value);
-  }
-
-  private ensureLengthIsLessThan30Characters(value: string): void {
-    if (value.length > 30) {
-      throw new InvalidArgumentError(`The Course Name <${value}> has more than 30 characters`);
-    }
   }
 }
--- src/Contexts/LLBE/Students/domain/TitleName.ts
+++ src/Contexts/LLBE/Students/domain/TitleName.ts
@@ -4,12 +4,5 @@ import { InvalidArgumentError } from '../../../Shared/domain/value-object/Invali
 export class TitleName extends StringValueObject {
   constructor(value: string) {
     super(value);
-    // this.ensureLengthIsLessThan30Characters(value);
-  }
-
-  private ensureLengthIsLessThan30Characters(value: string): void {
-    if (value.length > 30) {
-      throw new InvalidArgumentError(`The Title Name <${value}> has more than 30 characters`);
-    }
   }
 }
