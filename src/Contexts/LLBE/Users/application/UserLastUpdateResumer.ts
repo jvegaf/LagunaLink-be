287974e40acef5d2b03166f9eb49d48e3637f058
Jose Vega <josevega234@gmail.com>
2021-02-23 03:52:57 +0100

    actualizacion de usuario

---
+++ src/Contexts/LLBE/Users/application/UserLastUpdateResumer.ts
@@ -0,0 +1,21 @@
+import { UserRepository } from '../domain/UserRepository';
+import { User } from '../domain/User';
+import { UserId } from '../../Shared/domain/Users/UserId';
+import { ApplicationService } from '../../../Shared/domain/ApplicationService';
+
+export class UserLastUpdateResumer extends ApplicationService {
+  private repository: UserRepository;
+
+  constructor(repository: UserRepository) {
+    super();
+    this.repository = repository;
+  }
+
+  async run(userId: UserId): Promise<void> {
+
+    const user: User = await this.repository.search(userId) as User;
+
+    await this.repository.save(user.resumeUpdatedAt());
+
+  }
+}
