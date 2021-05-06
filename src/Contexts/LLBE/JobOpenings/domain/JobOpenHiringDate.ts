dacc1469193c7458d6f545aeadb7b98d538fa1e1
Jose Vega <josevega234@gmail.com>
2021-05-07 00:38:57 +0200

    eliminado el condicional de futuro en el value object hiringDate

--- src/Contexts/LLBE/JobOpenings/domain/JobOpenHiringDate.ts
+++ src/Contexts/LLBE/JobOpenings/domain/JobOpenHiringDate.ts
@@ -1,17 +1,7 @@
 import { DateValueObject } from '../../../Shared/domain/value-object/DateValueObject';
-import { InvalidArgumentError } from '../../../Shared/domain/value-object/InvalidArgumentError';
 
 export class JobOpenHiringDate extends DateValueObject {
   constructor(value: string) {
     super(value);
-    this.ensureDateIsFuture(value);
-  }
-
-  private ensureDateIsFuture(value: string): void {
-    const dateValue = new Date(value);
-    const today = new Date(Date.now());
-    if (today >= dateValue){
-      throw new InvalidArgumentError('Hiring date should be future');
-    }
   }
 }
