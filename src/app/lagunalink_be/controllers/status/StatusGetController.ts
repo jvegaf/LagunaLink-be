0d08fea2c71a00a97ce11a2892c823102d972e8f
Jose Vega <josevega234@gmail.com>
2021-06-04 18:55:06 +0200

    status endpoint

--- src/app/lagunalink_be/controllers/status/StatusGetController.ts
+++ src/app/lagunalink_be/controllers/status/StatusGetController.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from 'express';
-import httpStatus from 'http-status';
 import { Controller } from '../Controller';
 
 export default class StatusGetController implements Controller {
   async run(req: Request, res: Response) {
-    res.status(httpStatus.OK).send();
+    res.status(200).send({message: 'OK'});
   }
 }
